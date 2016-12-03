9dc69d524ddbcf7b92214c0ef625504dc15feb1d
Der Fu <fu@funami.de>
2016-12-03 23:08:50 +0100

    noConflict

--- addRatioClasses.js
+++ addRatioClasses.js
@@ -1,29 +1,30 @@
-$.fn.addRatioClasses = function(options){
-    options = $.extend({
-        addRatioClassToBody: true,
-        elList: []
-    }, options);
+(function($) {
 
-    var classOptions = options,
-        h = $(window).height(),
-        w = $(window).width();
+    $.fn.addRatioClasses = function(options) {
+        options = $.extend({
+            addRatioClassToBody: true,
+            elList: []
+        }, options);
 
-    if (classOptions.elList.length !== 0) {
-      $.each(classOptions.elList, function( i, v ) {
-            $(v).addClass('h' + $(v).height() + ' ' + 'w' + $(v).width())
-      });
+        var classOptions = options,
+            h = $(window).height(),
+            w = $(window).width();
+
+        if (classOptions.elList.length !== 0) {
+            $.each(classOptions.elList, function(i, v) {
+                $(v).addClass('h' + $(v).height() + ' ' + 'w' + $(v).width())
+            });
+        }
+
+        if (classOptions.addRatioClassToBody) {
+            if (w / h == 16 / 9) {
+                $('body').addClass('ratio_16_9');
+            } else if (w / h == 4 / 3) {
+                $('body').addClass('ratio_4_3');
+            } else {
+                $('body').addClass('noRatio');
+            }
+        }
+
     }
-
-    if(classOptions.addRatioClassToBody) {
-      if (w/h == 16 / 9) {
-        $('body').addClass('ratio_16_9');
-      }
-      else if (w/h == 4 / 3) {
-        $('body').addClass('ratio_4_3');
-      }
-      else {
-        $('body').addClass('noRatio');
-      }
-    }
-
-}
+}(jQuery));
